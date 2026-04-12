0a135769efd8b3dc5104b64515817f30b3e15ace
T-Ash-90 <thomasedwardash@pm.me>
2026-04-12 11:09:36 +0200

    readme placeholder and logo update

--- init.js
+++ init.js
@@ -12,6 +12,7 @@ function createWindow() {
     mainWindow = new BrowserWindow({
         width: 1200,
         height: 800,
+        icon: path.join(__dirname, '/frontend/assets/images/logo.png'),
         webPreferences: {
           nodeIntegration: true,
           contextIsolation: false,
